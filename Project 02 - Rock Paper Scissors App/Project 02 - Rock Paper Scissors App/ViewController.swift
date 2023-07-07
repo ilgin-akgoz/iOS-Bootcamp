1f81ea0c5764e6855f1631c9d6f4a44784531f25
ilgin-akgoz <82042228+ilgin-akgoz@users.noreply.github.com>
2023-07-07 23:34:25 +0300

    initial commit

---
+++ Project 02 - Rock Paper Scissors App/Project 02 - Rock Paper Scissors App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 02 - Rock Paper Scissors App
+//
+//  Created by Ilgın Akgöz on 7.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
